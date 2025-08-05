3c35171bcf293f10c4ffe59b8190bec41e1f71cf
Zukhra Esenkeldy <zuhraesenkeldieva14@gmail.com>
2025-08-05 08:43:59 +0500

    Постаралась исправить все ошибки в проекте

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -111,7 +111,7 @@ const HeroSection = () => {
       
       <div className="absolute inset-0 bg-black/40 z-10" />
       
-      <div className="absolute inset-0 z-20 flex flex-col justify-end md:justify-center lg:justify-end px-4 md:px-12 pb-16 sm:pb-20 md:pb-16 lg:pb-20">
+      <div className="absolute inset-0 z-20 flex flex-col justify-end md:justify-center lg:justify-end px-4 md:px-12 pb-12 sm:pb-20 md:pb-16 lg:pb-20">
         <div className="flex flex-col md:flex-row w-full items-start md:items-end justify-between gap-4 md:gap-8">
           <div className="md:max-w-[55%]">
             <h1 className="text-white text-2xl sm:text-3xl md:text-5xl font-light leading-tight mb-4 md:mb-0">
